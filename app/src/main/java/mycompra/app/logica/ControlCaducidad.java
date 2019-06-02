eb8bf76e6aef7b6d7f217405c8ec66b5767dc618
DarthegX <44780141+DarthegX@users.noreply.github.com>
2019-06-02 04:08:56 +0200

    caducidad

--- app/src/main/java/mycompra/app/logica/ControlCaducidad.java
+++ app/src/main/java/mycompra/app/logica/ControlCaducidad.java
@@ -1,7 +1,6 @@
 package mycompra.app.logica;
 
 import android.content.Context;
-import android.content.Intent;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,7 +12,11 @@ import java.util.Locale;
 import mycompra.app.dao.ProductoDAO;
 import mycompra.app.modelo.Producto;
 
-public class ControlCaducidad {
+public class ControlCaducidad
+{
+    public static ArrayList<Producto> prodsCaducados;
+    public static ArrayList<Producto> prodsCaducaHoy;
+    public static ArrayList<Producto> prodsCaduProxima;
 
     public static int MAX_CAD_DIAS = 2;
     private static ProductoDAO productoDAO;
@@ -44,14 +47,19 @@ public class ControlCaducidad {
         if (Integer.parseInt(dia) > Calendar.DATE && Integer.parseInt(mes) >= Calendar.MONTH)
         {
             // Alarma PRODUCTO CADUCADO
+            prodsCaducados.add(prod);
         }
         else if ((Integer.parseInt(dia) == Calendar.DATE && Integer.parseInt(mes) == Calendar.MONTH))
         {
             // Alarma PRODUCTO CADUCA HOY
+            prodsCaducaHoy.add(prod);
         }
         else if ((Calendar.DATE - (Integer.parseInt(dia)) == MAX_CAD_DIAS && Integer.parseInt(mes) == Calendar.MONTH))
         {
             // Alarma PRODUCTO CADUCA PRONTO
+            prodsCaduProxima.add(prod);
         }
     }
+
+
 }
